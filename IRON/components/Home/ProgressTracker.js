34482a83db298dd3f07e089d5ca8879125ed067a
Will <trainee.william.simms@sigmalabs.co.uk>
2023-01-26 15:19:11 +0000

    added pressables to progress tracker

--- IRON/components/Home/ProgressTracker.js
+++ IRON/components/Home/ProgressTracker.js
@@ -3,7 +3,24 @@ import { View, Text, Pressable } from "react-native";
 export default function ProgressTracker({ navigation }) {
   return (
     <View>
-      <Text>Progress Tracker</Text>
+      <Pressable
+        className="h-12 p-4 m-2 bg-green"
+        onPress={() => navigation.navigate("")}
+      >
+        <Text className="text-white">History</Text>
+      </Pressable>
+      <Pressable
+        className="h-12 p-4 m-2 bg-green"
+        onPress={() => navigation.navigate("")}
+      >
+        <Text className="text-white">Analytics</Text>
+      </Pressable>
+      <Pressable
+        className="h-12 p-4 m-2 bg-green"
+        onPress={() => navigation.navigate("")}
+      >
+        <Text className="text-white">PR List</Text>
+      </Pressable>
     </View>
   );
 }
