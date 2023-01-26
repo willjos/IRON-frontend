a05095959c12e9b65489cbfb8c6859e08272db5f
Will <trainee.william.simms@sigmalabs.co.uk>
2023-01-26 15:22:55 +0000

    made component files in progress folder

---
+++ IRON/components/Progress/Analytics.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Analytics() {
+  return (
+    <View>
+      <Text>Analytics</Text>
+    </View>
+  );
+}
---
+++ IRON/components/Progress/PRList.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function PRList() {
+  return (
+    <View>
+      <Text>PR List</Text>
+    </View>
+  );
+}
