552ccc585fe6ccd5c73b8e60160390f769479236
Will <trainee.william.simms@sigmalabs.co.uk>
2023-02-07 16:09:48 +0000

    PR list displayed

--- IRON/components/Progress/PRList.js
+++ IRON/components/Progress/PRList.js
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 
 export default function PRList({ userPRs }) {
   return (
-    <View>
-      {userPRs.map((item) => (
-        <Text>{item}</Text>
+    <ScrollView>
+      {Object.keys(userPRs).map((item, index) => (
+        <View key={index} className="p-4 m-2 bg-green">
+          <Text className="text-white font-semibold py-1">{item}</Text>
+          <Text className="text-white py-0.5">
+            {userPRs[item]["weight"]} kg x {userPRs[item]["reps"]}
+          </Text>
+          <Text className="text-white">{userPRs[item]["logged_at"]}</Text>
+        </View>
       ))}
-    </View>
+    </ScrollView>
   );
 }
