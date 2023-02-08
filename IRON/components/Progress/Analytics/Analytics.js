7052c370051dd37b86ee1f70084f3744245bd7a6
Will <trainee.william.simms@sigmalabs.co.uk>
2023-02-08 16:57:27 +0000

    graphing functionality added

--- IRON/components/Progress/Analytics/Analytics.js
+++ IRON/components/Progress/Analytics/Analytics.js
@@ -1,45 +1,46 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ScrollView, View, Text, Pressable } from "react-native";
 import VolumeProgressChart from "./VolumeProgressChart";
 
 export default function Analytics({ userHistory, userExerciseData }) {
   const [pressedExercise, setPressedExercise] = useState("");
-  const [pressedExerciseData, setPressedExerciseData] = useState([]);
   const userLogs = Object.keys(userHistory)
     .map((key) => userHistory[key])
     .flat();
   userLogs.forEach((set) => (set["volume"] = set["weight"] * set["reps"]));
-  // Each object in userLogs has a logged_at and volume property, these will be the data points for the graph
-
+  const pressedExerciseData = userLogs.filter(
+    (log) => log["exercise_name"] === pressedExercise
+  );
   const handleExercisePress = (exerciseName) => {
     setPressedExercise(exerciseName);
   };
-
-  useEffect(() => {
-    const newPressedExerciseData = userLogs.filter(
-      (log) => log["exercise_name"] === pressedExercise
-    );
-    setPressedExerciseData(newPressedExerciseData);
-    console.log(newPressedExerciseData);
-  }, [pressedExercise]);
-
   return (
     <View>
-      <View>
-        <VolumeProgressChart pressedExerciseData={pressedExerciseData} />
+      <View className="h-100 border-solid border-slate bg-grey">
+        <Text className="font-semibold text-2xl text-center m-4">
+          {pressedExercise ? pressedExercise : "Select an exercise"}
+        </Text>
+        {pressedExercise && pressedExerciseData.length ? (
+          <VolumeProgressChart pressedExerciseData={pressedExerciseData} />
+        ) : (
+          <Text className="font-semibold text-2xl text-center m-4">
+            No Data
+          </Text>
+        )}
       </View>
       <ScrollView>
-        {userExerciseData.map((exercise, index) => (
-          <Pressable
-            key={index}
-            className="h-12 p-4 m-2 bg-green"
-            onPress={() => {
-              handleExercisePress(exercise["exercise_name"]);
-            }}
-          >
-            <Text className="text-white">{exercise["exercise_name"]}</Text>
-          </Pressable>
-        ))}
+        {userExerciseData &&
+          userExerciseData.map((exercise, index) => (
+            <Pressable
+              key={index}
+              className="h-12 p-4 m-2 bg-green"
+              onPress={() => {
+                handleExercisePress(exercise["exercise_name"]);
+              }}
+            >
+              <Text className="text-white">{exercise["exercise_name"]}</Text>
+            </Pressable>
+          ))}
       </ScrollView>
     </View>
   );
--- IRON/components/Progress/Analytics/VolumeProgressChart.js
+++ IRON/components/Progress/Analytics/VolumeProgressChart.js
@@ -1,3 +1,74 @@
+import { Canvas, Path, Line, Skia, vec } from "@shopify/react-native-skia";
+import { curveBasis, line, scaleLinear, scaleTime } from "d3";
+import { View } from "react-native";
+
 export default function VolumeProgressChart({ pressedExerciseData }) {
-  return;
+  const GRAPH_HEIGHT = 400;
+  const GRAPH_WIDTH = 370;
+
+  const makeGraph = () => {
+    const min = Math.min(
+      ...pressedExerciseData.map((point) => point["volume"])
+    );
+    const max = Math.max(
+      ...pressedExerciseData.map((point) => point["volume"])
+    );
+    const getYAxis = scaleLinear().domain([0, max]).range([GRAPH_HEIGHT, 35]);
+    const getXAxis = scaleTime()
+      .domain([
+        new Date(pressedExerciseData[0]["logged_at"]),
+        new Date(
+          pressedExerciseData[pressedExerciseData.length - 1]["logged_at"]
+        ),
+      ])
+      .range([10, GRAPH_WIDTH - 10]);
+    const curvedLine = line()
+      .x((point) => getXAxis(new Date(point["logged_at"])))
+      .y((point) => getYAxis(point["volume"]))
+      .curve(curveBasis)(pressedExerciseData);
+    const skPath = Skia.Path.MakeFromSVGString(curvedLine);
+    return {
+      min,
+      max,
+      curve: skPath,
+    };
+  };
+  const graphData = makeGraph();
+  return (
+    <View>
+      {graphData && (
+        <Canvas
+          style={{
+            height: GRAPH_HEIGHT,
+            width: GRAPH_WIDTH,
+          }}
+        >
+          <Line
+            strokeWidth={1}
+            color="#353535"
+            p1={vec(10, 130)}
+            p2={vec(400, 130)}
+          />
+          <Line
+            strokeWidth={1}
+            color="#353535"
+            p1={vec(10, 250)}
+            p2={vec(400, 250)}
+          />
+          <Line
+            strokeWidth={1}
+            color="#353535"
+            p1={vec(10, 370)}
+            p2={vec(400, 370)}
+          />
+          <Path
+            path={graphData.curve}
+            color="#3C6E71"
+            strokeWidth={3}
+            style="stroke"
+          />
+        </Canvas>
+      )}
+    </View>
+  );
 }
