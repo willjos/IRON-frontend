5031449a30768b1fc2a807f4da63185a4d33e0fb
Will <trainee.william.simms@sigmalabs.co.uk>
2023-03-06 16:59:27 +0000

    live updates on user data added

--- IRON/App.js
+++ IRON/App.js
@@ -1,44 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import NavLogin from "./components/Navigation/NavLogin";
 import NavHome from "./components/Navigation/NavHomePage";
-import {
-  getUserExercises,
-  getUserHistory,
-  getUserWorkouts,
-  getUserPRs,
-} from "./Networking/APIRequests";
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState("");
-  const [userWorkoutData, setUserWorkoutData] = useState({});
-  const [userExerciseData, setUserExerciseData] = useState([]);
-  const [userHistory, setUserHistory] = useState({});
-  const [userPRs, setUserPRs] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      const userWorkoutData = await getUserWorkouts(currentUser);
-      const userExerciseData = await getUserExercises(currentUser);
-      const userHistory = await getUserHistory(currentUser);
-      const userPRs = await getUserPRs(currentUser);
-      setUserWorkoutData(userWorkoutData);
-      setUserExerciseData(userExerciseData);
-      setUserHistory(userHistory);
-      setUserPRs(userPRs);
-    })();
-  }, [currentUser]);
 
   return (
     <>
       {currentUser ? (
-        <NavHome
-          currentUser={currentUser}
-          setCurrentUser={setCurrentUser}
-          userWorkoutData={userWorkoutData}
-          userExerciseData={userExerciseData}
-          userHistory={userHistory}
-          userPRs={userPRs}
-        />
+        <NavHome currentUser={currentUser} setCurrentUser={setCurrentUser} />
       ) : (
         <NavLogin setCurrentUser={setCurrentUser} />
       )}
--- IRON/components/Home/HomePage.js
+++ IRON/components/Home/HomePage.js
@@ -1,11 +1,43 @@
 import { View, Text, Pressable } from "react-native";
-import { useState } from "react";
+import {
+  getUserWorkouts,
+  getUserExercises,
+  getUserHistory,
+  getUserPRs,
+} from "../../Networking/APIRequests";
 
-export default function HomePage({ navigation, currentUser, setCurrentUser }) {
+export default function HomePage({
+  navigation,
+  currentUser,
+  setCurrentUser,
+  setUserWorkoutData,
+  setUserExerciseData,
+  setUserHistory,
+  setUserPRs,
+}) {
+  const handleStartSessionPress = async () => {
+    const userWorkoutData = await getUserWorkouts(currentUser);
+    setUserWorkoutData(userWorkoutData);
+    navigation.navigate("Start Session");
+  };
+
+  const handleCreatePress = async () => {
+    const userExerciseData = await getUserExercises(currentUser);
+    setUserExerciseData(userExerciseData);
+    navigation.navigate("Create");
+  };
+
+  const handleProgressTrackerPress = async () => {
+    const userHistoryData = await getUserHistory(currentUser);
+    const userPRData = await getUserPRs(currentUser);
+    setUserHistory(userHistoryData);
+    setUserPRs(userPRData);
+    navigation.navigate("Progress Tracker");
+  };
+
   const handleLogoutPress = () => {
     setCurrentUser("");
   };
-
   return (
     <View className="h-full w-full justify-center bg-white">
       <Text className="text-lg text-left mx-2 py-2">
@@ -13,19 +45,16 @@ export default function HomePage({ navigation, currentUser, setCurrentUser }) {
       </Text>
       <Pressable
         className="h-12 p-4 m-2 bg-green"
-        onPress={() => navigation.navigate("Start Session")}
+        onPress={handleStartSessionPress}
       >
         <Text className="text-white">Start Session</Text>
       </Pressable>
-      <Pressable
-        className="h-12 p-4 m-2 bg-green"
-        onPress={() => navigation.navigate("Create")}
-      >
+      <Pressable className="h-12 p-4 m-2 bg-green" onPress={handleCreatePress}>
         <Text className="text-white">Create</Text>
       </Pressable>
       <Pressable
         className="h-12 p-4 m-2 bg-green"
-        onPress={() => navigation.navigate("Progress Tracker")}
+        onPress={handleProgressTrackerPress}
       >
         <Text className="text-white">Progress Tracker</Text>
       </Pressable>
